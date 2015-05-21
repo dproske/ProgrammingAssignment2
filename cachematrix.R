7fbf21739ebc195c4f2d260d9c92d5cdc8fbf44b
Daniel Proske <danielproske@DPMB.local>
2015-05-20 21:48:40 -0500

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() and cacheSolve() together provide a means for
+## caclulating and caching matrices and their inverses.
 
-## Write a short comment describing this function
+## makeCacheMatrix() takes a matrix as input and outputs a list
+## of functions that get and set the matrix and its inverse, 
+## Both of the set functions use the '<<-' operator so values can
+## be assigned to the x and inv variables in other environments. 
+## When created, the inv variable is set to NULL signifying that 
+## the inverse has not yet been calculated. Also, if the matrix
+## value changes (set() is called), the inverse is again set to 
+## NULL. This is important because the value of inv determines 
+## whether cacheSolve() recaculates the inverse or merely finds 
+## the cached value. We only want the inverse to be calculated 
+## when the matrix has changed.
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -18,7 +28,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() takes a list of functions defined in 
+## makeCacheMatrix() as input and calculates the inverse of the
+## matrix if it has not already been calculated (if inv is null)
+## or outputs the existing matrix inverse which has been cached.
 
 cacheSolve <- function(x, ...) {
     inv <- x$getInv()
@@ -31,3 +44,29 @@ cacheSolve <- function(x, ...) {
     x$setInv(inv)
     inv
 }
+
+
+## Below is a sample script for testing the the two functions.
+
+## INPUT
+##
+## m <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+## flist <- makeCacheMatrix(m)
+## cacheSolve(flist)
+
+## EXPECTED OUTPUT
+##
+##      [,1] [,2]
+## [1,]  -2  1.5
+## [2,]   1 -0.5
+
+## INPUT
+##
+## cacheSolve(flist)
+
+## EXPECTED OUTPUT
+##
+## getting cached data
+##      [,1] [,2]
+## [1,]  -2  1.5
+## [2,]   1 -0.5
